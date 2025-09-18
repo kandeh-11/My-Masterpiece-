e63a203b75e74f5915227ff6a8be6e8c65d2860e
kandeh-11 <ousmankandeh744@yahoo.com>
2025-09-18 12:05:22 -0700

    coded question 1 and 2

---
+++ week 4/loops.py
@@ -0,0 +1,20 @@
+#Ousman Kandeh
+'''
+larger_num = int(input("Enter a larger number: "))
+smaller_num = int(input("Enter a smaller number: "))
+
+
+num = 0
+while larger_num>smaller_num:
+    larger_num / 2
+    num += 1
+    print(f"number of times if halfed: {num}")
+
+'''
+
+#Question 2
+word = input("Enter a word: ")
+result = ""
+
+for i in range(1, len(word) - 1, 2):
+print(f"Output = {result}")
