6aca5579068384fac6b1938fef639c5e237df329
kandeh-11 <ousmankandeh744@yahoo.com>
2025-09-25 10:02:38 -0700

    new

--- week 4/loops.py
+++ week 4/loops.py
@@ -28,7 +28,7 @@ for num in range(37, 1051):
         print(num)
 '''
 
-  #Question 
+  #Question 4
 '''
 while True:
     user_input = input('Enter a letter: ')
@@ -50,6 +50,7 @@ for number in range(51, 518, 2):
 '''
 #Question 6
 total = 0
+'''
 while True:
     num = int(input("Enter an integer (negative number to stop): "))
     if num < 0:
@@ -57,3 +58,70 @@ while True:
     total += num  # add the number to the total if it's not negative
 
 print("The sum of the numbers is:", total)
+'''
+#Question 7
+'''
+n = int(input("Enter a positive integer: "))
+
+print("Hailstone sequence:")
+
+while n != 1:
+    print(n, end=" ")  # print current number
+    
+    if n % 2 == 0:  # even
+        n = n // 2
+    else:  # odd
+        n = 3 * n + 1
+
+print(1)
+'''
+
+#Question 8
+'''
+num = int(input("Enter an integer: "))
+
+print(f"Factors of {num} are:")
+
+# Loop through numbers from 1 to num
+for i in range(1, num + 1):
+    if num % i == 0:  # Check if i is a factor
+        print(i)
+'''
+#Question 9
+'''
+width = int(input("Enter rug width: "))
+length = int(input("Enter rug length: "))
+pattern = input("Enter a pattern character (e.g., *, #, @): ")
+
+print("\nHere is your rug:\n")
+
+# Print the rug
+for _ in range(length):
+    print(pattern * width)
+'''
+#Question 10
+'''
+argest_even = -1  # Default if no even numbers are entered
+
+while True:
+    num = int(input("Enter an integer (negative to stop): "))
+    
+    if num < 0:  # Stop if negative number entered
+        break
+    
+    if num % 2 == 0:  # Check if even
+        if num > largest_even:
+            largest_even = num
+
+print("Largest even number entered:", largest_even)
+'''
+# Question 11
+'''
+n = int(input("Enter an integer: "))
+
+total = 0
+for i in range(1, n + 1):
+    total += i ** 2
+
+print("Sum of squares up to", n, "is:", total)
+'''
